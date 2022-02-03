3e253e1641b24f5f59cd1f365a06dd30b0650224
Ogun <omt66@hotmail.com>
2022-02-02 22:33:36 -0800

    Adjusting

--- src/main/java/edu/nu/csc272/week1/StringDemo.java
+++ src/main/java/edu/nu/csc272/week1/StringDemo.java
@@ -12,9 +12,9 @@ public class StringDemo {
 //       demo1();
 //       demo2();
 //       demo3();
-//        demo4();
-//        demo5();
-//        demo6();
+//       demo4();
+//       demo5();
+//       demo6();
         demo7();
     }
 
